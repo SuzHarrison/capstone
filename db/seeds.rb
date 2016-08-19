01a8d5509f58ea93c71c2f474b7d109959475be8
Suzanne Harrison <harrison_family@hotmail.com>
2016-08-18 18:46:03 -0700

    added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,7 +77,96 @@ providers = [
     bed_count: 5,
     average_cost: 2400
   },
-
+  {
+    yelp_id: "queen-anne-manor-senior-living-seattle",
+    bed_count: 4,
+    average_cost: 1900
+  },
+  {
+    yelp_id: "fairwinds-redmond-redmond",
+    bed_count: 10,
+    average_cost: 2200
+  },
+  {
+    yelp_id: "aegis-on-madison-seattle",
+    bed_count: 10,
+    average_cost: 2200
+  },
+  {
+    yelp_id: "covenant-shores-mercer-island-2",
+    bed_count: 4,
+    average_cost: 2900
+  },
+  {
+    yelp_id: "fairwinds-brittany-park-woodinville",
+    bed_count: 7,
+    average_cost: 2200
+  },
+  {
+    yelp_id: "fairwinds-brighton-court-lynnwood",
+    bed_count: 12,
+    average_cost: 2200
+  },
+  {
+    yelp_id: "overlake-terrace-retirement-community-redmond",
+    bed_count: 8,
+    average_cost: 2300
+  },
+  {
+    yelp_id: "aegis-of-lodge-kirkland",
+    bed_count: 20,
+    average_cost: 2900
+  },
+  {
+    yelp_id: "regency-newcastle-newcastle",
+    bed_count: 4,
+    average_cost: 1900
+  },
+  {
+    yelp_id: "aegis-of-shoreline-shoreline-3",
+    bed_count: 17,
+    average_cost: 2700
+  },
+  {
+    yelp_id: "patriots-glen-assisted-living-bellevue",
+    bed_count: 10,
+    average_cost: 2400
+  },
+  {
+    yelp_id: "aegis-of-bellevue-bellevue",
+    bed_count: 19,
+    average_cost: 2900
+  },
+  {
+    yelp_id: "aegis-of-redmond-redmond",
+    bed_count: 10,
+    average_cost: 2900
+  },
+  {
+    yelp_id: "el-dorado-west-burien",
+    bed_count: 18,
+    average_cost: 2100
+  },
+  {
+    yelp_id: "aegis-of-issaquah-issaquah",
+    bed_count: 15,
+    average_cost: 2800
+  },
+  {
+    yelp_id: "aegis-of-lynnwood-lynnwood",
+    bed_count: 20,
+    average_cost: 2800
+  },
+  {
+    yelp_id: "mountlake-terrace-plaza-mountlake-terrace-2",
+    bed_count: 7,
+    average_cost: 2700
+  },
+  {
+    yelp_id: "the-cottages-carepartners-management-group-mill-creek",
+    bed_count: 9,
+    average_cost: 2400
+  },
 ]
 
 providers.each do |p|
